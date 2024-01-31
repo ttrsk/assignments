86fe2c48a26b50a48769edf0399f4d04df2440ed
Nikita Nyanchuk-Tatarskiy <tatarskiy.ch@gmail.com>
2024-01-31 22:56:16 +0100

    Initial boilerplate generated with Spring Initializr

---
+++ wipro.calculator/src/test/java/tatarskiy/assignments/wipro/calculator/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tatarskiy.assignments.wipro.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
